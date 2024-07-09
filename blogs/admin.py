cb5ba482cc3f072551a4a618c60f81f6f112b7ff
aditya-1123 <gaikwadaditya1123@gmail.com>
2024-07-09 18:54:33 +0530

    blogs and search features

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Category, Blog
+from django.http import HttpRequest
+from .models import Category, Blog, About, SocialLink
 
 class BlogAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
@@ -7,6 +8,15 @@ class BlogAdmin(admin.ModelAdmin):
     search_fields = ('id', 'title', 'category__category_name', 'status')
     list_editable = ('is_featured',)
 
+class AboutAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request):
+        count = About.objects.all().count()
+        if count == 0:
+            return True
+        return False
+
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Blog, BlogAdmin)
+admin.site.register(About, AboutAdmin)
+admin.site.register(SocialLink)
