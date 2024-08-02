0b47f3bb8143271b1c2b2e57bbee070c6ec6db52
aditya-1123 <gaikwadaditya1123@gmail.com>
2024-08-02 17:10:33 +0530

    resolved error in search functionality

--- blogs/views.py
+++ blogs/views.py
@@ -43,8 +43,9 @@ def blogs(request, slug):
 
 def search(request):
     keyword = request.GET.get('keyword')
-    
+    print(keyword)
     blogs = Blog.objects.filter(Q(title__icontains=keyword)| Q(short_description__icontains=keyword) | Q(blog_body__icontains=keyword), status='Published')
+
     context = {
         'blogs': blogs,
         'keyword': keyword
